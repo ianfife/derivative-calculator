39ebe7915d31bd020a4676e16b70aabfb828ac3a
Mast3rGenius <iandfife@gmail.com>
2021-09-30 11:52:58 -0500

    Update

--- derivativeCalculator/Function.java
+++ derivativeCalculator/Function.java
@@ -1,10 +1,10 @@
 package derivativeCalculator;
 
 public class Function {
-  private String function;
-  private String function2;
-  private String derivative;
-  private String derivative2;
+  //String input
+  private final String function;
+  //function with any whitespace removed
+  private String noSpaces = "";
 
   public Function() {
     this("x^2 + 1");
@@ -12,9 +12,16 @@ public class Function {
   
   public Function(String a) {
     function = a;
-    for (int i = 0; i < function.length(); i++) {
-      if (function.substring(i, i+1).equals(" ")) {
-        function2 = function.substring(0, i) + function.substring(i + 1);
+    //Check for spaces, if none, set noSpaces and skip whitespace removal
+    if (!function.contains(" ")) {
+      noSpaces = function;
+    }
+    else {
+      //Remove whitespace from string
+      for (int i = 0; i < function.length(); i++) {
+        if (function.charAt(i) != ' ') {
+          noSpaces = noSpaces + function.charAt(i);
+        }
       }
     }
   }
@@ -24,25 +31,30 @@ public class Function {
   }
 
   public String removeConstants() {
-    derivative = function2;
-    derivative2  = "";
+    //noSpaces without constants
+    String derivative = noSpaces;
+    //Parsed derivative
+    String derivativeParsed = "";
 
     for (int j = 0; j < derivative.length(); j++) {
       boolean notConstant = false;
       int a = 0;
       //Account for - in the future
-      for (int i = j; i < derivative.length() && !derivative.substring(i,i+1).equals("+"); i++) {
-        if (derivative.substring(i, i+1).equals("x"))
+      for (int i = j; i < derivative.length() && (derivative.charAt(i) != '+' || derivative.charAt(i) != '-'); i++) {
+        if (derivative.charAt(i) == 'x') {
           notConstant = true;
-        if ((i == derivative.length()-1 || derivative.substring(i+1, i+2).equals("+")) && notConstant)
+        }
+        if ((i == derivative.length()-1 || derivative.charAt(i + 1) == '+'|| derivative.charAt(i + 1) == '-') && notConstant) {
           a = i;
+        }
         a++;
       }
-      if (notConstant)
-        derivative2 = derivative2 + derivative.substring(j, a);
+      if (notConstant) {
+        derivativeParsed = derivativeParsed + derivative.substring(j, a);
+      }
       j += a;
     }
-    return derivative2;
+    return derivativeParsed.toString();
   }
 
 }
--- derivativeCalculator/FunctionModifier.java
+++ derivativeCalculator/FunctionModifier.java
@@ -7,14 +7,11 @@ class FunctionModifier {
   public static void main(String[] args) throws IOException {
     Scanner scan = new Scanner(System.in);
     System.out.println("Enter function (Ex: x^2 + 1)");
-    String func = scan.next().toLowerCase();
+    String func = scan.nextLine().toLowerCase();
     scan.close();
 
     Function func1 = new Function(func);
-    Function func2 = new Function();
     System.out.println(func1.removeConstants());
     System.out.println(func1);
-    System.out.println(func2.removeConstants());
-    System.out.println(func2);
   }
 }
